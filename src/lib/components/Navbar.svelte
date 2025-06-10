bb74debd4f07f9111bdfe161688f0bac5042b687
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-06-10 16:39:13 +0800

    refactor(components): indicate current page on the navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,10 @@
-<script>
+<script lang="ts">
+  import { page } from "$app/state";
+
   import { Section } from "$lib/components";
+
+  const pathname = $derived(page.url.pathname);
+
   const pages = [
     {
       display: "about",
@@ -14,12 +19,18 @@
       href: "/contact",
     },
   ];
+
+  function linkClasses(href: string) {
+    return href === pathname ? "link link-current" : "link";
+  }
 </script>
 
 <Section title="navbar">
   <div class="navbar">
     {#each pages as page (page.display)}
-      <div class="link-container"><a href={page.href}>{page.display}</a></div>
+      <div class="link-container">
+        <a href={page.href} class={linkClasses(page.href)}>{page.display}</a>
+      </div>
     {/each}
   </div>
 </Section>
@@ -36,8 +47,19 @@
     flex-grow: 1;
   }
 
-  .link-container > a {
+  .link {
     font-size: 16pt;
     text-align: center;
+    text-decoration: none;
+
+    color: var(--ctp-macchiato-text);
+    &:hover {
+      color: var(--ctp-macchiato-blue);
+    }
+  }
+
+  .link-current {
+    font-weight: bold;
+    color: var(--ctp-macchiato-mauve);
   }
 </style>
