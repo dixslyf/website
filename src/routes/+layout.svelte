46f0d92b652d2ff9e8b22ec41395396873de364c
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-06-10 16:41:48 +0800

    feat: animate main page content when page changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,31 @@
 <script>
   import "./styles.css";
 
-  import { Title } from "$lib/components";
-  import { Navbar } from "$lib/components";
+  import { page } from "$app/state";
+  import { fly } from "svelte/transition";
+
+  import { Title, Navbar, Stack, Group } from "$lib/components";
 
   const { children } = $props();
 </script>
 
 <div class="root-container">
-  <div class="content-container">
-    <Title />
-    <Navbar />
-    {@render children()}
+  <div class="subroot-container">
+    <Stack>
+      <Title />
+      <Navbar />
+      <div class="content-container-wrapper">
+        {#key page.url.pathname}
+          <div
+            class="content-container"
+            in:fly={{ x: "30%", duration: 500 }}
+            out:fly={{ x: "-30%", duration: 500 }}
+          >
+            {@render children()}
+          </div>
+        {/key}
+      </div>
+    </Stack>
   </div>
 </div>
 
@@ -22,7 +36,16 @@
     padding: 32pt 0;
   }
 
-  .content-container {
+  .subroot-container {
     width: 70%;
+  }
+
+  .content-container-wrapper {
+    position: relative;
+  }
+
+  .content-container {
+    position: absolute;
+    inset: 0;
   }
 </style>
