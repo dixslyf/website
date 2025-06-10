277e60451490e811de7c13fd0c1be70e0d25bece
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-06-10 16:36:35 +0800

    refactor(components): add group and stack components
    Similar to Mantine's Group and Stack. These are really just for
    convenience.

---
+++ src/lib/components/Group.svelte
@@ -0,0 +1,18 @@
+<script>
+  const { class: className = "", style = "", children } = $props();
+</script>
+
+<div class="group {className}" {style}>{@render children()}</div>
+
+<style>
+  .group {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: var(--wrap, nowrap);
+    justify-content: var(--justify, flex-start);
+    align-items: var(--align, center);
+
+    gap: var(--gap, 8px);
+    padding: var(--padding, 0);
+  }
+</style>
--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -2,3 +2,5 @@ export { default as Title } from "./Title.svelte";
 export { default as Navbar } from "./Navbar.svelte";
 export { default as Section } from "./Section.svelte";
 export { default as TextCaret } from "./TextCaret.svelte";
+export { default as Group } from "./Group.svelte";
+export { default as Stack } from "./Stack.svelte";
