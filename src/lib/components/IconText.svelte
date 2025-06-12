5b32ced3e5a6fa848ce6cf26090095ac42ad1366
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-06-12 12:24:48 +0800

    refactor(components): add component for icon and text

---
+++ src/lib/components/IconText.svelte
@@ -0,0 +1,34 @@
+<script>
+  const props = $props();
+</script>
+
+<div class="root">
+  {#if props.icon}
+    <span class="icon">{@render props.icon()}</span>
+  {/if}
+  <span class="label">{props.label}</span>
+</div>
+
+<style>
+  .root {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 0.5em;
+
+    padding: 0.4em 0.6em;
+
+    border-radius: 12px;
+
+    background-color: var(--ctp-macchiato-surface0);
+  }
+
+  .icon {
+    color: var(--ctp-macchiato-flamingo);
+  }
+
+  .label {
+    font-size: 10pt;
+    font-weight: 600;
+  }
+</style>
--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -4,3 +4,4 @@ export { default as Section } from "./Section.svelte";
 export { default as TextCaret } from "./TextCaret.svelte";
 export { default as Group } from "./Group.svelte";
 export { default as Stack } from "./Stack.svelte";
+export { default as IconText } from "./IconText.svelte";
