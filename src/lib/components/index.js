723a9833cb1751bd9d893c3a51a41236c4e556b1
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-06-19 23:30:23 +0800

    refactor: remove dead code
    Group and Stack weren't flexible enough unfortunately, and it seems
    pretty hard to make them easily stylable with Svelte (seems to be a
    general problem with Svelte).
    
    Typewriter transition had been replaced with the Typewriter class.

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -2,8 +2,6 @@ export { default as Title } from "./Title.svelte";
 export { default as Navbar } from "./Navbar.svelte";
 export { default as Section } from "./Section.svelte";
 export { default as TextCaret } from "./TextCaret.svelte";
-export { default as Group } from "./Group.svelte";
-export { default as Stack } from "./Stack.svelte";
 export { default as IconText } from "./IconText.svelte";
 export { default as LanguagesBar } from "./LanguagesBar.svelte";
 export { default as Devicon } from "./Devicon.svelte";
--- src/lib/transitions/index.js
+++ src/lib/transitions/index.js
@@ -1,2 +1,1 @@
-export { typewriter } from "./typewriter";
 export { blink } from "./blink";
