bf9dd0a5a999ae088b98e46acbed73173dcf2c2f
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-06-21 23:59:35 +0800

    feat: init vue project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
