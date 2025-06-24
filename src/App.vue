a882273b9cb7af2720f67c3078f371a01a57a9b2
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-06-25 00:08:12 +0800

    feat: sketch main page layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+  import { useRouter, RouterView, useRoute } from "vue-router";
+
+  import { Navbar } from "@/components";
+  import { Stack, Center } from "@/components/primitives";
+
+  const router = useRouter();
+  const route = useRoute();
+
+  const routes = router.options.routes.map(({ path, name }) => ({
+    path,
+    name: name?.toString() || "undefined",
+  }));
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">About</RouterLink>
-    </nav>
-  </header>
+  <div :class="$style.root">
+    <Center gutters="0">
+      <div :class="$style.contentRoot">
+        <Stack gap="var(--space-m)">
+          <header>
+            <Navbar
+              :routes="routes"
+              :currentRoutePath="route.path"
+            />
+          </header>
 
-  <RouterView />
+          <RouterView />
+        </Stack>
+      </div>
+    </Center>
+  </div>
 </template>
 
-<style scoped></style>
+<style module>
+  .root {
+    padding-block: var(--space-m-l);
+    padding-inline: var(--space-m);
+  }
+
+  .contentRoot {
+    max-width: var(--content-max-width);
+  }
+</style>
