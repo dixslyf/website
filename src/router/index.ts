eeda92d7ef5788adb23f0dd8af51986509b55503
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-06-25 00:03:03 +0800

    feat(views): add placeholder views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import About from "@/views/About.vue";
+import { About, Projects, Contact } from "@/views";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +8,16 @@ const router = createRouter({
       path: "/",
       name: "about",
       component: About,
+    },
+    {
+      path: "/projects",
+      name: "projects",
+      component: Projects,
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: Contact,
     },
   ],
 });
---
+++ src/views/index.ts
@@ -0,0 +1,3 @@
+export { default as About } from "./About.vue";
+export { default as Projects } from "./Projects.vue";
+export { default as Contact } from "./Contact.vue";
