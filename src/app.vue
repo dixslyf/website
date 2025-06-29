9a7a3849f18079ef605ccea181510da927a1d8f0
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-06-29 16:39:46 +0800

    refactor: show splash only once per session

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
   const showSplash = ref(true);
+
+  onBeforeMount(() => {
+    const hasSeenSplash = sessionStorage.getItem("hasSeenSplash");
+    if (hasSeenSplash) {
+      showSplash.value = false;
+    }
+  });
+
   onMounted(() => {
+    if (!showSplash.value) {
+      return;
+    }
+
     // Hide the splash after a delay.
     setTimeout(() => {
       showSplash.value = false;
+      sessionStorage.setItem("hasSeenSplash", "true");
     }, 2000);
   });
 </script>
