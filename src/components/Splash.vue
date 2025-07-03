0978ec927e42aab748d63d0dd9b96324d60a71cb
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-07-03 15:56:39 +0800

    fix(components): re-introduce splash screen

--- src/components/Splash.vue
+++ src/components/Splash.vue
@@ -1,4 +1,29 @@
 <script setup lang="ts">
+  import { onMounted, ref } from "vue";
+
+  import { AnimatePresence, motion } from "motion-v";
+
+  import { Stack, Center } from "@/components/primitives";
+  import { NameDisplay } from "@/components";
+  import { splashProps, fadeProps } from "@/utils/animations";
+
+  const showSplash = ref(true);
+  onMounted(() => {
+    const hasSeenSplash = sessionStorage.getItem("hasSeenSplash");
+    if (hasSeenSplash) {
+      showSplash.value = false;
+      return;
+    }
+
+    showSplash.value = true;
+
+    // Hide the splash after a delay.
+    setTimeout(() => {
+      showSplash.value = false;
+      sessionStorage.setItem("hasSeenSplash", "true");
+    }, 3000);
+  });
+
   // For avoiding layout shift during initial load.
   const showNameDisplay = ref(false);
   onMounted(() => {
@@ -7,19 +32,26 @@
 </script>
 
 <template>
-  <div :class="$style.splashRoot">
-    <Stack :class="$style.splashStack">
-      <Center>
-        <Motion
-          v-if="showNameDisplay"
-          v-bind="fadeProps()"
-          asChild
-        >
-          <NameDisplay :class="$style.splashContent" />
-        </Motion>
-      </Center>
-    </Stack>
-  </div>
+  <AnimatePresence client:load>
+    <motion.div
+      client:load
+      v-if="showSplash"
+      v-bind="splashProps()"
+      :class="$style.splashRoot"
+    >
+      <Stack :class="$style.splashStack">
+        <Center>
+          <Motion
+            v-if="showNameDisplay"
+            v-bind="fadeProps()"
+            asChild
+          >
+            <NameDisplay :class="$style.splashContent" />
+          </Motion>
+        </Center>
+      </Stack>
+    </motion.div>
+  </AnimatePresence>
 </template>
 
 <style module>
