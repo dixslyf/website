da969cfeb8f9d82db80186dd92854e743abfc2b0
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-07-04 01:36:37 +0800

    fix: fix build failure

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -1,9 +1,13 @@
 import vikeVue from "vike-vue/config";
+import * as vercel from "@vite-plugin-vercel/vike/config";
 import type { Config } from "vike/types";
 
 export default {
   title: "dixslyf",
   description: "dixslyf's website",
-  extends: vikeVue as typeof vikeVue,
-  prerender: true,
+  extends: [vikeVue, vercel],
+  prerender: {
+    enable: true,
+    keepDistServer: true, // Needed for Vercel plugin.
+  },
 } satisfies Config;
