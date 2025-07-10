da3d2ed8547d87be57dcb45817105d2ee800d27b
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-07-10 18:29:17 +0800

    refactor(components): decompose meta into smaller components

---
+++ src/components/meta/index.ts
@@ -0,0 +1,4 @@
+export { default as Meta } from "./Meta.astro";
+export { default as BaseMeta } from "./BaseMeta.astro";
+export { default as FaviconMeta } from "./FaviconMeta.astro";
+export { default as OpenGraphMeta } from "./OpenGraphMeta.astro";
